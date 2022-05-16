e6ddab77dd1a1784e77a75dd4fce23092b341747
Carlos Ramos <Carlos.Ramos.Dev@outlook.com>
2022-05-15 19:27:23 -0600

    Ajustes

--- app/src/main/java/com/gingerbread/app/Main.java
+++ app/src/main/java/com/gingerbread/app/Main.java
@@ -1,26 +1,23 @@
 package com.gingerbread.app;
 
-import com.gingerbread.lib.table.element;
-import com.gingerbread.lib.table.search;
-
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the number of element: ");
-        String name = scanner.nextLine();
-        element element  = search.searchElementByName(name);
-
-        System.out.println("Element: " + element.getSymbol());
-        System.out.println("Name: " + element.getName());
-        System.out.println("Description: " + element.getDescription());
-        System.out.println("Metal: " + element.isMetal());
-        System.out.println("Nonmetal: " + element.isNonmetal());
-        System.out.println("Halogen: " + element.isHalogen());
-        System.out.println("Noble Gas: " + element.isNobleGas());
-        System.out.println("Transition Metal: " + element.isTransitionMetal());
-        System.out.println("Lanthanide: " + element.isLanthanide());
-        System.out.println("Actinide: " + element.isActinide());
+        System.out.println("--- Herramienta para la resolución de problemas de la materia de Química  ---");
+        System.out.println("Seleccione la opción deseada:");
+        System.out.println("1. Buscar elemento");
+        System.out.println("2-9. Próximamente");
+        System.out.println("10. Salir");
+        switch (inputs.inputIntByRange(scanner, 1, 10)) {
+            case 1 -> {
+                SearchElement.mainPage(scanner);
+            }
+            case 10 -> {
+                System.out.println("Gracias por usar el programa");
+            }
+            default -> System.out.println("Próximamente");
+        }
     }
 }
