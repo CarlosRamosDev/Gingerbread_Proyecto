89b020e83904aad27c9b07d99d1a7e4e8dd26e90
Carlos Ramos <Carlos.Ramos.Dev@outlook.com>
2022-05-31 18:13:06 -0600

    Mejoras en el menu principal

--- app/src/main/java/com/gingerbread/app/Main.java
+++ app/src/main/java/com/gingerbread/app/Main.java
@@ -6,26 +6,34 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
+    private static final String[] options = {"Herramienta para la resolución de problemas de la materia de Química","Buscar elemento", "", "", "", "", "", "", "", "Créditos", "Salir"};
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("--- Herramienta para la resolución de problemas de la materia de Química  ---");
-        System.out.println("Seleccione la opción deseada:");
-        System.out.println("1. Buscar elemento");
-        System.out.println("2-8. Próximamente");
-        System.out.println("9. Créditos");
-        System.out.println("10. Salir");
-        switch (inputs.inputIntByRange(scanner, 1, 10)) {
-            case 1 -> {
-                SearchElement.mainPage(scanner);
+        boolean exit = false;
+        do {
+            printOptions();
+            switch (inputs.inputIntByRange(scanner, 1, 10)) {
+                case 1 -> {
+                    SearchElement.mainPage(scanner);
+                }
+                case 9 -> common.copyrightAndAuthor();
+                case 10 -> exit = true;
+                default -> System.out.println("Próximamente");
             }
-            case 9 -> {
-                common.copyrightAndAuthor();
+        } while (!exit);
+        System.out.println("Gracias por usar el programa");
+    }
+    public static void printOptions() {
+        for (int i = 0; i < options.length; i++) {
+            if (i == 0) {
+                System.out.println(options[i]);
+            } else {
+                if (options[i] != "") {
+                    System.out.println(i + ". " + options[i]);
+                }
             }
-            case 10 -> {
-                System.out.println("Gracias por usar el programa");
-            }
-            default -> System.out.println("Próximamente");
         }
+        System.out.print("Seleccione una opción: ");
     }
 }
 
