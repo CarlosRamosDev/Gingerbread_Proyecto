c3277f6e1c4141cd58fcbfcb2fd154c6b7b24f06
Carlos Ramos <Carlos.Ramos.Dev@outlook.com>
2022-06-01 23:36:16 -0600

    Panel de usuarios

--- src/main/java/com/gingerbread/Main.java
+++ src/main/java/com/gingerbread/Main.java
@@ -26,6 +26,7 @@ public class Main {
         do {
             try {
                 switch (Integer.parseInt(Menus.getMainMenu().printMenu(scanner))) {
+                    case 7 -> usersMenu(scanner, acces);
                     case 8 -> Menus.getCreditsMenu().printMenu(scanner);
                     case 9 -> exit = true;
                     default -> throw new Exception("Opción no válida");
@@ -37,5 +38,44 @@ public class Main {
             }
         } while (!exit);
         System.out.println("Hasta pronto!");
+    }
+
+    public static void usersMenu(Scanner scanner, Acces acces) {
+        boolean exit = false;
+        do {
+            try {
+                switch (Integer.parseInt(Menus.getUserMenu().printMenu(scanner))) {
+                    case 1 -> {
+                        System.out.println("Usuarios registrados: ");
+                        for (User user : acces.loadUsers()) {
+                            System.out.println(user.getName());
+                        }
+                    }
+                    case 2 -> {
+                        System.out.print("Introduce el nombre del usuario: ");
+                        String name = scanner.nextLine();
+                        System.out.print("Introduce la contraseña: ");
+                        String password = scanner.nextLine();
+                        System.out.println("Añadiendo usuario...");
+                        acces.addUser(new User(name, password));
+                        System.out.println("Usuario añadido");
+                    }
+                    case 3 -> {
+                        System.out.print("Introduce el nombre del usuario: ");
+                        String name = scanner.nextLine();
+                        System.out.print("Introduce la contraseña: ");
+                        String password = scanner.nextLine();
+                        System.out.println("Eliminando usuario...");
+                        acces.removeUser(new User(name, password));
+                        System.out.println("Usuario eliminado");
+                    }
+                    case 4 -> exit = true;
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Error: Introduce un número entero como opción");
+            } catch (Exception e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        } while (!exit);
     }
 }
--- src/main/java/com/gingerbread/Menus.java
+++ src/main/java/com/gingerbread/Menus.java
@@ -14,7 +14,7 @@ public class Menus {
         options[4] = "opción 4";
         options[5] = "opción 5";
         options[6] = "opción 6";
-        options[7] = "opción 7";
+        options[7] = "Acceder al sistema de usuarios";
         options[8] = "Creitós";
         options[9] = "salir";
         menu.setOptions(options);
@@ -35,6 +35,20 @@ public class Menus {
                 -----------------------------------------------------------------
                 Pulsa enter para volver al menú principal...""";
         menu.setBottomText(credits);
+        menu.setPauseByEnter(0);
+        return menu;
+    }
+
+    public static Menu getUserMenu() {
+        Menu menu = new Menu();
+        menu.setTitle("Administración de usuarios");
+        String[] options = new String[5];
+        options[1] = "Listar usuarios";
+        options[2] = "Añadir usuario";
+        options[3] = "Eliminar usuario";
+        options[4] = "Salir";
+        menu.setOptions(options);
+        menu.setBottomText("Selecciona una opción: ");
         menu.setPauseByEnter(0);
         return menu;
     }
