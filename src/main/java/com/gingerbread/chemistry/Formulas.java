35e7a4cb214d1efb23fc83a83dd93d2b98ca6564
yis19 <jesusalejandrot958@gmail.com>
2022-06-11 19:32:37 -0600

    Primer avance de las Formulas

--- src/main/java/com/gingerbread/chemistry/Formulas.java
+++ src/main/java/com/gingerbread/chemistry/Formulas.java
@@ -1,4 +1,41 @@
 package com.gingerbread.chemistry;
 
 public class Formulas {
+    //TEORIA ONDULATORIA DE LA LUZ
+    //Logitud de onda
+    public static double longDeOnda (double f){
+        double longOnda;
+        double c = 300000000.00;
+        longOnda = (c / f);
+        return longOnda;
+    }
+
+    //Frecuencia en Hz
+    public static double frecuencia (double l){
+        double frecu;
+        double c = 300000000.00;
+        frecu = (c/l);
+        return frecu;
+    }
+
+    //ENERGÍA CUANTIZADA Y FOTONES
+    //Energía emitida de la radiación electromagnética
+    public static double energia (double f){
+        double energia;
+        double h=0.0000000000000000000000000000000006626;
+        energia = h*f;
+        return energia;
+    }
+
+    //Frecuencia energia
+    public static double frecuenciaEnergia (double e){
+        double frecuenciaE;
+        double h=0.0000000000000000000000000000000006626;
+        frecuenciaE = e/h;
+        return frecuenciaE;
+    }
+
+    //Modelo de Bohr
+    //Energía por Bohr
+    // public static double energiaBohr (double )
 }
