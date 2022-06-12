888a45b37fdc944c275499252bb9ac3421fa0cdd
Carlos Ramos <CarlosIRamosV@Outlook.com>
2022-06-12 02:03:34 -0600

    Subtemas Plantilla

--- src/main/java/com/gingerbread/chemistry/Topic_3.java
+++ src/main/java/com/gingerbread/chemistry/Topic_3.java
@@ -1,38 +1,10 @@
 package com.gingerbread.chemistry;
 
 public class Topic_3 {
-
-
-    private String topic1_3;
-    private String topic1_3_1;
-
-    public Topic_3() {
-    }
-
-    public Topic_3(String topic1_3, String topic1_3_1) {
-        this.topic1_3 = topic1_3;
-        this.topic1_3_1 = topic1_3_1;
-    }
-
-    public String getTopic1_3() {
-        return topic1_3;
-    }
-
-    public void setTopic1_3(String topic1_3) {
-        this.topic1_3 = topic1_3;
-    }
-
-    public String getTopic1_3_1() {
-        return topic1_3_1;
-    }
-
-    public void setTopic1_3_1(String topic1_3_1) {
-        this.topic1_3_1 = topic1_3_1;
-    }
-
-    public void imprimirTema1_3() {
-        topic1_3 = """
-                1.3 Teoría atómica de Bohr
+    public static Topic Topic() {
+        Topic topic = new Topic();
+        topic.setName("1.3 Teoría atómica de Bohr");
+        topic.setDescription("""
                 El modelo de Bohr está basado en los siguientes postulados, que son válidos para átomos con un solo electrón como el hidrógeno y permitió explicar sus espectros de emisión y absorción.
                 Postulados:
                 1. Primer Postulado: Estabilidad del Electrón
@@ -43,13 +15,9 @@ public class Topic_3 {
                 3. Tercer Postulado: Niveles Estacionarios de Energía
                 Un electrón que se mueva en una de esas órbitas permitidas no irradia energía electromagnética, aunque está siendo acelerado constantemente por las fuerzas atractivas al núcleo. Por ello, su energía total permanece constante.
                 4. Cuarto Postulado: Emisión y Absorción de Energía
-                Si un electrón que inicialmente se mueve en una órbita de energía Ei cambia discontinuamente su movimiento de forma que pasa a otra órbita de energía Ef se emite o absorbe energía electromagnética para compensar el cambio de la energía total. La frecuencia ν de la radiación es igual a la cantidad (Ei – Ef) dividida por la constante de Planck h.""";
-        System.out.println(topic1_3);
-    }
-
-    public void imprimirTema1_3_1() {
-        topic1_3_1 = """
-                1.3.1 Teoría atómica de Bohr-Sommerfeld
+                Si un electrón que inicialmente se mueve en una órbita de energía Ei cambia discontinuamente su movimiento de forma que pasa a otra órbita de energía Ef se emite o absorbe energía electromagnética para compensar el cambio de la energía total. La frecuencia ν de la radiación es igual a la cantidad (Ei – Ef) dividida por la constante de Planck h."""
+        );
+        topic.addSubtopic(new Topic("1.3.1 Teoría atómica de Bohr-Sommerfeld", """
                 En 1916, el físico alemán Arnold Sommerfeld modificó el modelo de Bohr en el sentido que las órbitas permitidas para los electrones debían ser elípticas más que circulares.\s
                 El modelo de Bohr - Sommerfeld es un buen ejemplo de un tipo de evolución de las teorías científicas: un modelo antiguo es modificado para incorporar y explicar datos nuevos.
                 El modelo mecánico cuántico. La teoría de Bohr - Sommerfeld fue una pieza maestra de simplicidad, que explicó satisfactoriamente el espectro del hidrógeno. Sin embargo, tenía una contradicción fatal: funcionaba sólo con átomos de hidrógeno e iones que contenían sólo un electrón.
@@ -61,8 +29,9 @@ public class Topic_3 {
                 l = 0 se denominarían posteriormente orbitales "s" o Sharp
                 l = 1 se denominarían "p" o principal.
                 l = 2 se denominarían "d" o diffuse.
-                l = 3 se denominarían "f" o fundamental.""";
-        System.out.println(topic1_3_1);
+                l = 3 se denominarían "f" o fundamental."""
+        ));
+        return topic;
     }
 }
 
